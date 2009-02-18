28fb7dba78488e904357baaf133cbcbcc3db4438
troy <troy@zinc.dc.resophonic.com>
2009-02-18 17:56:24 -0500

    code generation now looking not too shabby.

--- templates/generate.py
+++ templates/generate.py
@@ -19,34 +19,40 @@ KAMASU_MAX_ARRAY_DIM = 6
 
 def forall(**kwargs):
     if len(kwargs) == 1:
-        for k, v in kwargs.iteritems():
-            for value in v:
-                yield [(k, value)]
+        k = kwargs.keys()[0]
+        v = kwargs[k]
+        for value in v:
+            yield [(k, value)]
     else:
-        for k, v in kwargs.iteritems():
-            for value in v:
-                newargs = kwargs.copy()
-                del(newargs[k])
-                for nextthing in forall(**newargs):
-                    yield[(k,value)] + nextthing
+        k = kwargs.keys()[0]
+        v = kwargs[k]
+        for value in v:
+            newargs = kwargs.copy()
+            del(newargs[k])
+            for nextthing in forall(**newargs):
+                yield[(k,value)] + nextthing
 
-stuff = [{ 'src' : 'templates/unary_array_transforms.h',
-           'dest' : 'src/unary_array_transforms.h.generated',
-           'next' : forall(OP=functions)},
+one_to_n = range(1,KAMASU_MAX_ARRAY_DIM)
 
-         { 'src' : 'templates/unary_array_op.cu',
-           'dest' : 'src/unary_array_op.cu.generated',
-           'next' : forall(OP=functions, N=range(1,KAMASU_MAX_ARRAY_DIM)) },
+stuff = [
 
-         { 'src' : 'templates/unary_array_grammar.hpp',
-           'dest' : 'include/resophonic/kamasu/unary_array_grammar.hpp',
-           'next' : [{'functions' : functions}]},
+    { 'src' : 'templates/unary_array_transforms.h',
+      'dest' : 'src/unary_array_transforms.h.generated',
+      'next' : forall(OP=functions, KAMASU_MAX_ARRAY_DIM=[KAMASU_MAX_ARRAY_DIM])},
+ 
+    { 'src' : 'templates/unary_array_op.cu',
+      'dest' : 'src/unary_array_op.cu.generated',
+      'next' : forall(OP=functions, N=one_to_n) },
 
-         { 'src' : 'templates/unary_array_op.h',
-           'dest' : 'src/unary_array_op.h.generated',
-           'next' : forall(OP=functions, N=range(1,KAMASU_MAX_ARRAY_DIM)) }
+    { 'src' : 'templates/unary_array_grammar.hpp',
+      'dest' : 'include/resophonic/kamasu/unary_array_grammar.hpp',
+      'next' : [{'functions' : functions}]},
 
-         ]
+    { 'src' : 'templates/unary_array_op.h',
+      'dest' : 'src/unary_array_op.h.generated',
+      'next' : forall(OP=functions, N=one_to_n) }
+
+    ]
 
 pattern = re.compile(r'\/\*(.*?)\*\/', re.DOTALL)
 
@@ -65,8 +71,6 @@ for target in stuff:
     print >>ofile, '//\n//\n// this is a generated file\n//\n//\n'
     for env in target['next']:
         env = dict(env)
-        env['KAMASU_MAX_ARRAY_DIM'] = KAMASU_MAX_ARRAY_DIM
-        print env
         def r(x):
             matched = x.group(1)
             res = eval(matched, env)

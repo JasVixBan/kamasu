1fd0caacb83768fd8f07c058dd5e8353775113b4
troy <troy@zinc.dc.resophonic.com>
2009-04-09 12:07:32 -0400

    benchmark for slightly more complicated expression (a + 3) * (b + 7)

---
+++ benchmark/matrix_multiply_2.cpp
@@ -0,0 +1,63 @@
+#include <resophonic/kamasu.hpp>
+#include "benchmark.hpp"
+
+#include <boost/numeric/ublas/matrix.hpp>
+
+using resophonic::kamasu::array;
+namespace ublas = boost::numeric::ublas;
+
+struct matrix_multiply_2 
+{
+  static void usage() {
+    std::cout << "n x\nn = size of a(n,n) and b(n,n)\nx = number of times to multiply\n";
+  }
+
+  struct kamasu : benchmark<kamasu>
+  {
+    unsigned n, x;
+    kamasu(unsigned n_, unsigned x_) : n(n_), x(x_) { }
+
+    array<float> a, b, c;
+
+    void start() 
+    { 
+      a = array<float>(n,n); 
+      b = array<float>(n,n); 
+    }
+    void stop() 
+    { 
+    }
+    int main() 
+    { 
+      for (unsigned i = 0; i<x; i++)
+	c = (a + 3.0f) * (b + 7.0f);
+      return 0;
+    }
+  };
+
+  struct cpu : benchmark<cpu>
+  {
+    unsigned n, x;
+    cpu(unsigned n_, unsigned x_) : n(n_), x(x_) { }
+
+    ublas::matrix<float> a, b, c;
+
+    void start() 
+    { 
+      a = ublas::matrix<float>(n,n);
+      b = ublas::matrix<float>(n,n);
+    }
+    void stop() 
+    { 
+    }
+    int main() 
+    { 
+      for (unsigned i=0; i<x; i++)
+	c = prod(a * 3.0f, b * 7.0f);
+      return 0;
+    }
+  };
+};
+
+MAIN(2);
+

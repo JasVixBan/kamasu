dfa76d7cab0e0b8dc2aa3d474e3e762bffa9d0da
troy <troy@zinc.dc.resophonic.com>
2009-04-12 16:15:21 -0400

    fix timing calculation

--- benchmark/benchmark.hpp
+++ benchmark/benchmark.hpp
@@ -15,8 +15,6 @@ struct benchmark
   timespec starttime, endtime, fullstarttime, fullendtime;
   rusage start_rusage, stop_rusage;
 
-  uint64_t kamasu_wall_ms, cpu_wall_ms; 
-
   Derived& derived() { return *(static_cast<Derived*>(this)); }
 
   void start()
@@ -35,24 +33,31 @@ struct benchmark
     clock_gettime(CLOCK_REALTIME, &fullendtime);
   }
 
-  int run()
+  int run(int argc, const char** argv)
   {
     start();
     derived().main();
     stop();
     double user = (stop_rusage.ru_utime.tv_sec - start_rusage.ru_utime.tv_sec);
-    user += double(stop_rusage.ru_utime.tv_usec - start_rusage.ru_utime.tv_usec) / 10e+06;
+    user += (double(stop_rusage.ru_utime.tv_usec) - double(start_rusage.ru_utime.tv_usec)) * 1.0e-06;
 
     double sys = (stop_rusage.ru_stime.tv_sec - start_rusage.ru_stime.tv_sec);
-    sys += double(stop_rusage.ru_stime.tv_usec - start_rusage.ru_stime.tv_usec) / 10e+06;
+    sys += (double(stop_rusage.ru_stime.tv_usec) - double(start_rusage.ru_stime.tv_usec)) * 1.0e-06;
 
     double wall = (endtime.tv_sec - starttime.tv_sec);
-    wall += double (endtime.tv_nsec - starttime.tv_nsec) / 10e+09;
+    wall += double (endtime.tv_nsec - starttime.tv_nsec) * 1.0e-09;
 
-    double full = (fullendtime.tv_sec - fullstarttime.tv_sec);
-    full += double (fullendtime.tv_nsec - fullstarttime.tv_nsec) / 10e+09;
+    double full = fullendtime.tv_sec - fullstarttime.tv_sec;
+    double full_ns = (fullendtime.tv_nsec - fullstarttime.tv_nsec) * 1.0e-09;
+    full += full_ns;
 
-    std::cerr << boost::format("%10s %10s %10s %10s\n") % "user" % "sys" % "wall" % "full";
+    //    std::cout << "full: " << fullstarttime.tv_sec << " " << fullstarttime.tv_nsec << " ... " 
+    //	      << fullendtime.tv_sec << " " << fullendtime.tv_nsec << "\n"
+    //	      << "full_ns:" << full_ns << "\n";
+
+    //std::cerr << boost::format("%10s %10s %10s %10s\n") % "user" % "sys" % "wall" % "full";
+    for (int i=0; i<argc; i++)
+      std::cout << argv[i] << " ";
     std::cout << boost::format("%10f %10f %10f %10f\n") % user % sys % wall % full;
     return 0;
   }
@@ -75,7 +80,7 @@ struct kamasu
 };
 
 template <typename Test, typename Selector>
-int main_impl1(int argc, char** argv)
+int main_impl1(int argc, const char** argv)
 {
   if (argc < 1)
     {
@@ -84,13 +89,12 @@ int main_impl1(int argc, char** argv)
       return 1;
     }
   unsigned i = boost::lexical_cast<unsigned>(argv[1]);
-  std::cout << "Args: " << i << "\n";
   typename Selector::template impl<Test>::type mark(i);
-  return mark.run();
+  return mark.run(argc, argv);
 }
 
 template <typename Test, typename Selector>
-int main_impl2(int argc, char** argv)
+int main_impl2(int argc, const char** argv)
 {
   if (argc < 2)
     {
@@ -101,13 +105,12 @@ int main_impl2(int argc, char** argv)
 
   unsigned i = boost::lexical_cast<unsigned>(argv[1]);
   unsigned j = boost::lexical_cast<unsigned>(argv[2]);
-  std::cout << "Args: " << i << " " << j << "\n";
   typename Selector::template impl<Test>::type mark(i,j);
-  return mark.run();
+  return mark.run(argc, argv);
 }
 
 #define MAIN(N)							\
-  int main(int argc, char** argv)				\
+  int main(int argc, const char** argv)				\
   {								\
     return main_impl ## N <NAME, TYPE>(argc, argv);		\
   }

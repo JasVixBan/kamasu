78e7e7d93bac74d7b56f75a0cc3fa50f09cc68f6
troy <troy@zinc.dc.resophonic.com>
2009-04-14 18:36:10 -0400

    fancy compile flags.

--- CMake/CMakeCUDAInformation.cmake
+++ CMake/CMakeCUDAInformation.cmake
@@ -4,6 +4,9 @@ set(CMAKE_CUDA_COMPILE_OBJECT
   "<CMAKE_CUDA_COMPILER> <DEFINES> -c <SOURCE> -o <OBJECT> -Xcompiler <FLAGS>"
   )
 
+set(CMAKE_CUDA_FLAGS_DEBUG "-G -g" CACHE STRING "Generate debuggable device and host code")
+set(CMAKE_CUDA_FLAGS_RELEASE "-O" CACHE STRING "Opt flags for nvcc")
+
 find_library(CUDA_RT_LIBRARY
   cudart
   PATHS ${CUDA_DIRECTORY}/lib
@@ -26,6 +29,7 @@ find_library(CUDA_DRIVER_LIBRARY
   cuda
   DOC "cuda driver library"
   )
+
 
 message("
    CMAKE_CUDA_COMPILER    = ${CMAKE_CUDA_COMPILER}

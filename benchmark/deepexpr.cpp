6db1cb48024081da3d981c4e4051851febd2b724
troy <troy@zinc.dc.resophonic.com>
2009-04-15 22:25:19 -0400

    benchmark shows how slow non numerically intensive stuff is

--- benchmark/deepexpr.cpp
+++ benchmark/deepexpr.cpp
@@ -11,52 +11,69 @@ struct deepexpr
 
   struct kamasu : benchmark<kamasu>
   {
-    unsigned n;
-    kamasu(unsigned n_) : n(n_) { }
-
+    unsigned n, x;
     array<float> a, b, c, d, e;
+    std::vector<float> randvec;
+
+    kamasu(unsigned n_, unsigned x_) : n(n_), x(x_) 
+    { 
+      randvec.resize(n);
+      for (unsigned i=0; i<n; i++)
+	randvec[i] = rand();
+    }
 
     void start() 
     { 
-      a = array<float>(n); 
-      b = array<float>(n); 
-      c = array<float>(n); 
-      d = array<float>(n); 
+      a << randvec;
+      b << randvec;
+      c << randvec;
+      d << randvec;
     }
     void stop() 
     { 
     }
     int main() 
     { 
-      e = a * 2.0f; 
-      e += (d * 2.0f);
-      e += (b * 2.0f) / (c * 2.0f);
+      for (unsigned i=0; i<x; i++)
+	e = a * 2.0f + b / 2.0f + c * 2.0f + d / 2.0f;
       return 0;
+    }
+    int verify(const std::vector<float>& r)
+    {
+      std::vector<float> v;
+      e >> v;
+      for (unsigned i=0; i<n; i++)
+	ENSURE_DISTANCE(v[i], r[i], 1.0e-5);
     }
   };
 
   struct cpu : benchmark<cpu>
   {
-    unsigned n;
-    cpu(unsigned n_) : n(n_) { }
+    unsigned n, x;
+    std::vector<float> a, b, c, d, result, randvec;
 
-    std::vector<float> a, b, c, d, e;
+    cpu(unsigned n_, unsigned x_) : n(n_), x(x_)
+    {
+      a.resize(n);
+      for (unsigned i=0; i<n; i++)
+	a[i] = rand();
+    }
 
     void start() 
     { 
-      a.resize(n);
-      b.resize(n);
-      c.resize(n);
-      d.resize(n);
-      e.resize(n);
+      b = a;
+      c = a;
+      d = a;
+      result.resize(n);
     }
     void stop() 
     { 
     }
     int main() 
     { 
-      for (unsigned i=0; i<n; i++)
-	e[i] = (a[i] * 2.0f) + (b[i] * 2.0f) / (c[i] * 2.0f) + (d[i] * 2.0f);
+      for (unsigned i=0; i<x; i++)
+	for (unsigned i=0; i<n; i++)
+	  result[i] = a[i] * 2.0f + b[i] / 2.0f + c[i] * 2.0f + d[i] / 2.0f;
       return 0;
     }
   };

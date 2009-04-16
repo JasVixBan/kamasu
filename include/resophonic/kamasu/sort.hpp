1bd6bb1889a60236d4ce55556f254ab6ddd959c3
troy <troy@zinc.dc.resophonic.com>
2009-04-15 23:46:22 -0400

    sort header

---
+++ include/resophonic/kamasu/sort.hpp
@@ -0,0 +1,18 @@
+#ifndef RESOPHONIC_KAMASU_SORT_HPP_INCLUDED
+#define RESOPHONIC_KAMASU_SORT_HPP_INCLUDED
+
+#include <resophonic/kamasu/array.hpp>
+
+namespace resophonic {
+  namespace kamasu {
+
+    template <typename T>
+    void sort(array<T>&);
+
+  }
+}
+
+
+
+#endif
+

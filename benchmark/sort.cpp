9bc3d44d46f1a9dbace1abb1cc619cd00254b574
troy <troy@zinc.dc.resophonic.com>
2009-04-16 00:07:44 -0400

    sort benchmark.  breakeven at ~3 million floats.

---
+++ benchmark/sort.cpp
@@ -0,0 +1,100 @@
+#include <resophonic/kamasu.hpp>
+#include <resophonic/kamasu/sort.hpp>
+#include "benchmark.hpp"
+#include <test/I3Test.h>
+#include <boost/numeric/ublas/matrix.hpp>
+
+using resophonic::kamasu::array;
+namespace ublas = boost::numeric::ublas;
+namespace t = resophonic::kamasu::testing;
+
+struct sort : suite<sort>
+{
+  static void usage() 
+  {
+    std::cout << "n x\nn = size of a(n,n) and b(n,n)\nx = number of times to multiply\n";
+  }
+
+
+  struct kamasu : benchmark<kamasu>
+  {
+    array<float> a;
+    std::vector<float> indata, myres;
+    unsigned n, x;
+
+    kamasu(unsigned n_, unsigned x_) 
+      : n(n_), x(x_), indata(n_)
+    { 
+      t::rng.seed(13);
+      for (unsigned i=0; i<n; i++)
+	  indata[i] = rand();
+    }
+
+    void start() 
+    { 
+      a << indata;
+    }
+
+    int main() 
+    { 
+      resophonic::kamasu::sort(a);
+      return 0;
+    }
+
+    void stop() 
+    { 
+      a >> myres;
+    }
+
+    void verify(const std::vector<float>& v)
+    {
+      for (int i=0; i<n-1; i++)
+	{
+	  ENSURE(myres[i] <= myres[i+1]);
+	  ENSURE(v[i] <= v[i+1]);
+	}
+    }
+  };
+
+  struct cpu : benchmark<cpu>
+  {
+    std::vector<float> result;
+    unsigned n, x;
+
+    cpu(unsigned n_, unsigned x_) 
+      : n(n_), x(x_), result(n_)
+    { 
+      t::rng.seed(13);
+      for (unsigned i=0; i<n; i++)
+	result[i] = rand();
+    }
+
+    void start() 
+    { 
+    }
+    void stop() 
+    { 
+    }
+    int main() 
+    { 
+      std::sort(result.begin(), result.end());
+      return 0;
+    }
+  };
+};
+
+MAIN();
+
+
+
+
+// externally
+
+// number of arguments
+// types of arguments
+
+
+// internally cpu vs kamasu
+
+// type of result
+// verify function
